6eebcf0845770f32ad88a68d1f519e136ed972f3
songgaoshuai <gssong@qysoft.cn>
2022-08-08 18:03:27 +0800

    切换bpmn.js 集成activiti设计器

--- ruoyi-ui/src/views/workflow/model/index2.vue
+++ ruoyi-ui/src/views/workflow/model/index2.vue
@@ -88,35 +88,21 @@
           :page.sync="queryParams.pageNum"
           :limit.sync="queryParams.pageSize"
           @pagination="getList" />
-           <!-- 添加模型对话框 -->
-        <el-dialog title="新增模型" :visible.sync="open" width="650px" append-to-body>
-          <el-form ref="form" :model="form" :rules="rules" label-width="100px">
-             <el-form-item label="模型名称：" prop="name"  >
-                <el-input v-model="form.name"  maxlength="20" show-word-limit/>
-            </el-form-item>
-            <el-form-item label="标识Key：" prop="key">
-                <el-input v-model="form.key" maxlength="20" show-word-limit/>
-            </el-form-item>
-            <el-form-item label="备注：" prop="description">
-                <el-input type="textarea" v-model="form.description" maxlength="30" show-word-limit></el-input>
-            </el-form-item>
-          </el-form>
-          <div slot="footer" class="dialog-footer">
-            <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button>
-            <el-button @click="cancel">取 消</el-button>
-          </div>
-        </el-dialog>
         <!-- 设计流程 -->
-        <design ref="designModel" :modelId="modelId"/>
+        <el-dialog title="设计模型" :before-close="handleClose" :visible.sync="bpmnJsModelVisible"
+        v-if="bpmnJsModelVisible" fullscreen :modal-append-to-body='false'>
+            <bpmnJs ref="bpmnJsModel" @close-bpmn="closeBpmn" :categorysBpmn="categorysBpmn" :modelId="modelId"/>
+        </el-dialog>
     </div>
 </template>
 
 <script>
 import {list,add,del,deploy} from "@/api/workflow/model";
-import Design from './design'
+import BpmnJs from './bpmnJs'
 export default {
+    dicts: ['act_category'],
     name: 'Model', // 和对应路由表中配置的name值一致
-    components: {Design},
+    components: {BpmnJs},
     data() {
         return {
             //按钮loading
@@ -136,7 +122,7 @@ export default {
             // 总条数
             total: 0,
             // 是否显示弹出层
-            open: false,
+            bpmnJsModelVisible: false,
             // 模型定义表格数据
             modelList: [],
             // 查询参数
@@ -158,9 +144,11 @@ export default {
               ],
             },
             modelId: null, // 模型id
+            categorys:[]
         }
     },
     created() {
+      this.categorysBpmn = this.dict.type.act_category
       this.getList();
     },
     methods: {
@@ -183,12 +171,12 @@ export default {
       },
       //分页
       getList(){
-          this.loading = true;
-          list(this.queryParams).then(response => {
-            this.modelList = response.rows;
-            this.total = response.total;
-            this.loading = false;
-          })
+        this.loading = true;
+        list(this.queryParams).then(response => {
+          this.modelList = response.rows;
+          this.total = response.total;
+          this.loading = false;
+        })
       },
       /** 删除按钮操作 */
       handleDelete(row) {
@@ -218,23 +206,8 @@ export default {
          });
       },
       handleAdd(){
-        this.reset();
-        this.open = true;
-        this.title = "添加测试单表";
-      },
-      // 表单重置
-      reset() {
-        this.form = {
-          name: undefined,
-          key: undefined,
-          description: undefined,
-        };
-        this.resetForm("form");
-      },
-      // 取消按钮
-      cancel() {
-        this.open = false;
-        this.reset();
+        this.modelId = "new"
+        this.bpmnJsModelVisible = true
       },
       /** 提交按钮 */
       submitForm() {
@@ -251,10 +224,26 @@ export default {
             }
         });
       },
-      // 设计流程
+      // 打开设计流程
       clickDesign(id) {
-          this.modelId = id
-          this.$refs.designModel.visible = true
+        this.modelId = id
+        this.bpmnJsModelVisible = true
+      },
+      // 关闭设计流程
+      closeBpmn(){
+        this.getList()
+        this.bpmnJsModelVisible = false
+      },
+      handleClose() {
+        this.$confirm('请记得点击保存按钮，确定关闭设计窗口?', '确认关闭',{
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.bpmnJsModelVisible = false
+          // 刷新数据
+          this.getList()
+        }).catch(() => {})
       },
       // 导出流程模型
       clickExportZip(data){
---
+++ ruoyi-workflow/src/main/java/com/ruoyi/workflow/activiti/ModelSaveRestResource.java
@@ -0,0 +1,161 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ruoyi.workflow.activiti;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.ruoyi.common.annotation.Anonymous;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.annotation.RepeatSubmit;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.R;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.exception.ServiceException;
+import com.ruoyi.workflow.common.constant.ActConstant;
+import org.activiti.editor.constants.ModelDataJsonConstants;
+import org.activiti.engine.ActivitiException;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Model;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.PNGTranscoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+/**
+ * @author Tijs Rademakers
+ */
+@RestController
+@Anonymous
+public class ModelSaveRestResource extends BaseController implements ModelDataJsonConstants {
+
+  protected static final Logger LOGGER = LoggerFactory.getLogger(ModelSaveRestResource.class);
+
+  @Autowired
+  private RepositoryService repositoryService;
+
+  @Resource
+  private ObjectMapper objectMapper;
+
+    @Log(title = "新建模型", businessType = BusinessType.INSERT)
+    @RepeatSubmit
+    @PostMapping
+    public R<Model> add(@RequestBody Map<String ,Object> modelData) {
+        try {
+            int version = 0;
+            String key = modelData.get("key").toString();
+            String name = modelData.get("name").toString();
+            String description = modelData.get("description").toString();
+
+            Model checkModel = repositoryService.createModelQuery().modelKey(key).singleResult();
+            if(ObjectUtil.isNotNull(checkModel)){
+                return R.fail("模型key已存在",null);
+            }
+            // 1. 初始空的模型
+            Model model = repositoryService.newModel();
+            model.setKey(key);
+            model.setName(name);
+            model.setVersion(version);
+
+            // 封装模型json对象
+            ObjectNode objectNode = objectMapper.createObjectNode();
+            objectNode.put(ModelDataJsonConstants.MODEL_NAME, name);
+            objectNode.put(ModelDataJsonConstants.MODEL_REVISION, version);
+            objectNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION, description);
+            model.setMetaInfo(objectNode.toString());
+            // 保存初始化的模型基本信息数据
+            repositoryService.saveModel(model);
+
+            // 封装模型对象基础数据json串
+            // {"id":"canvas","resourceId":"canvas","stencilset":{"namespace":"http://b3mn.org/stencilset/bpmn2.0#"},"properties":{"process_id":"未定义"}}
+            ObjectNode editorNode = objectMapper.createObjectNode();
+            ObjectNode stencilSetNode = objectMapper.createObjectNode();
+            stencilSetNode.put("namespace", ActConstant.NAMESPACE);
+            editorNode.replace("stencilset", stencilSetNode);
+            // 标识key
+            ObjectNode propertiesNode = objectMapper.createObjectNode();
+            propertiesNode.put("process_id", key);
+            propertiesNode.put("name", name);
+            editorNode.replace("properties", propertiesNode);
+            repositoryService.addModelEditorSource(model.getId(), editorNode.toString().getBytes(StandardCharsets.UTF_8));
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new ServiceException(e.getMessage());
+        }
+        return R.ok();
+    }
+
+  /**
+   * @Description: 编辑模型
+   * @param: modelId
+   * @param: values
+   * @return: void
+   * @author: gssong
+   * @Date: 2022/8/8
+   */
+  @Log(title = "编辑模型", businessType = BusinessType.UPDATE)
+  @RequestMapping(value="/model/{modelId}/save", method = RequestMethod.PUT)
+  @ResponseStatus(value = HttpStatus.OK)
+  public void saveModel(@PathVariable String modelId, @RequestParam MultiValueMap<String, String> values) {
+    try {
+
+      Model model = repositoryService.getModel(modelId);
+
+      ObjectNode modelJson = (ObjectNode) objectMapper.readTree(model.getMetaInfo());
+
+      modelJson.put(MODEL_NAME, values.getFirst("name"));
+      modelJson.put(MODEL_DESCRIPTION, values.getFirst("description"));
+      model.setMetaInfo(modelJson.toString());
+      model.setName(values.getFirst("name"));
+      model.setVersion(model.getVersion() + 1 ); // 每次保存把版本更新+1
+      // 获取唯一标识key
+      String key = objectMapper.readTree(values.getFirst("json_xml")).get("properties").get("process_id").textValue();
+      model.setKey(key);
+
+
+      repositoryService.saveModel(model);
+
+      repositoryService.addModelEditorSource(model.getId(), values.getFirst("json_xml").getBytes(StandardCharsets.UTF_8));
+
+      InputStream svgStream = new ByteArrayInputStream(values.getFirst("svg_xml").getBytes(StandardCharsets.UTF_8));
+      TranscoderInput input = new TranscoderInput(svgStream);
+
+      PNGTranscoder transcoder = new PNGTranscoder();
+      // Setup output
+      ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+      TranscoderOutput output = new TranscoderOutput(outStream);
+
+      // Do the transformation
+      transcoder.transcode(input, output);
+      final byte[] result = outStream.toByteArray();
+      repositoryService.addModelEditorSourceExtra(model.getId(), result);
+      outStream.close();
+
+    } catch (Exception e) {
+      LOGGER.error("Error saving model", e);
+      throw new ActivitiException("Error saving model", e);
+    }
+  }
+}
